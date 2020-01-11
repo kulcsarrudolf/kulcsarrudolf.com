93446338f709101ef75b398ebc0421b8b13ffad3
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2020-01-11 12:22:43 +0200

    Initial revision

---
+++ contact/contact.php
@@ -0,0 +1,32 @@
+<?php
+// if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['message']) ){
+if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message']) ){
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	//$tel = $_POST['tel'];    
+	$message = nl2br($_POST['message']);
+	
+	$to = "kulcsarrudolf@gmail.com";	
+	$from = $email;
+	$subject = 'New message - '.$name;
+	// $message = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.'<br><b>Tel:</b> '.$tel.'<br><br> <p>'.$message.'</p>';
+	$message = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.'<br><br> <p>'.$message.'</p>';
+
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+	$headers .= 'From: "My Website Contact Form" <'.$from.'>' . "\r\n";
+	$headers .= "Reply-To: ".$to."\r\n";
+	$headers .= "Return-Path: ".$to."\r\n";
+
+	echo $to;
+	echo $subject;
+	echo $message;
+	echo $headers;
+
+	if(mail($to, $subject, $message, $headers)){
+		echo '<p class="err">Message sent!</p>';
+	} else {
+		echo '<p class="err">Error!</p>';
+	}
+}
+?>
---
+++ js/nav.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){ 
+
+    $('#btnContact').click(function(){ 
+        //$("html, body").animate({ scrollTop: 0 }, 600); 
+        //return false;
+        
+        $('html, body').animate({
+            scrollTop: $("#contact-me").offset().top
+        }, 2000);
+        
+        
+        //alert("test");
+
+    }); 
+
+    
+});
+
+function goToSection(sectionId){
+    $('html, body').animate({
+        scrollTop: $(sectionId).offset().top
+    }, 1500);
+}
