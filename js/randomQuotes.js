c8ee2111c7af4412306be93be1af3bd79945e3cd
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2020-01-22 19:28:44 +0200

    Added new quotes

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -10,7 +10,9 @@ var listOfQuotes =  [
     new Quote("Through hard work, perseverance and a faith in God, you can live your dreams.","Ben Carson"),
     new Quote("Everyday life is like programming, I guess. If you love something you can put beauty into it.", "Donald Knuth"),
     new Quote("Slowing down is sometimes the best way to speed up.", "Mike Vance"),
-    new Quote("Focus on one thing, make it your priority, and stick with it no matter what!", "Tyler Perry")
+    new Quote("Focus on one thing, make it your priority, and stick with it no matter what!", "Tyler Perry"),
+    new Quote("Perseverance is not a long race; it is many short races one after the other.", "Walter Elliot"),
+    new Quote("If you can't fly then run, if you can't run then walk, if you can't walk then crawl, but whatever you do you have to kepp moving forward.", "Martin Luther King Jr.")
 ]
 
 var randomNumber = Math.floor(Math.random() * listOfQuotes.length);
