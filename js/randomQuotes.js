58c2ec25af9798f70f2ebc2bd38baa9d7452ecf3
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2020-08-26 15:45:56 +0300

    Random qoutes update

--- js/randomQuotes.js
+++ js/randomQuotes.js
@@ -1,33 +1,41 @@
-class Quote {
-    constructor(qoute, author){
-        this.qoute = qoute;
-        this.author = author;
-    }
-}
-
-var listOfQuotes =  [
-    new Quote("If you cannot do great things, do small things in a great way.", "Napoleon Hill"),
-    new Quote("Through hard work, perseverance and a faith in God, you can live your dreams.","Ben Carson"),
-    new Quote("Everyday life is like programming, I guess. If you love something you can put beauty into it.", "Donald Knuth"),
-    new Quote("Slowing down is sometimes the best way to speed up.", "Mike Vance"),
-    new Quote("Focus on one thing, make it your priority, and stick with it no matter what!", "Tyler Perry"),
-    new Quote("Perseverance is not a long race; it is many short races one after the other.", "Walter Elliot"),
-    new Quote("There are two kinds of decisions - the right decision and a lesson learned.", "Simon Sinek"),
-    new Quote("If you can't fly then run, if you can't run then walk, if you can't walk then crawl, but whatever you do you have to kepp moving forward.", "Martin Luther King Jr.")
+var listOfQuotes = [
+    {
+        qoute: "If you cannot do great things, do small things in a great way.",
+        author: "Napoleon Hill",
+    },
+    {
+        qoute: "Through hard work, perseverance and a faith in God, you can live your dreams.",
+        author: "Ben Carson",
+    },
+    {
+        qoute: "Everyday life is like programming, I guess. If you love something you can put beauty into it.",
+        author: "Donald Knuth",
+    },
+    {
+        qoute: "Slowing down is sometimes the best way to speed up.",
+        author: "Mike Vance",
+    },
+    {
+        qoute: "Focus on one thing, make it your priority, and stick with it no matter what!",
+        author: "Tyler Perry",
+    },
+    {
+        qoute: "Perseverance is not a long race; it is many short races one after the other.",
+        author: "Walter Elliot",
+    },
+    {
+        qoute: "There are two kinds of decisions - the right decision and a lesson learned.",
+        author: "Simon Sinek",
+    },
+    {
+        qoute: "If you can't fly then run, if you can't run then walk, if you can't walk then crawl, but whatever you do you have to kepp moving forward.",
+        author: "Martin Luther King Jr.",
+    },
 ]
 
 var randomNumber = Math.floor(Math.random() * listOfQuotes.length);
 var item = listOfQuotes[randomNumber];
-
-function getRandomQuote(sel){
-    if(sel === "author"){
-        return document.write((item.author));
-    }else{
-        return document.write((item.qoute));
-    }
-}
 window.onload = function loadQoute() {
-    const randomQoute = "<p>" + getRandomQuote() + "<span>" + getRandomQuote("author") +"</span>" + "</p>";
-    document.getElementById("qoute-content").innerHTML = "There are two kinds of decisions - the right decision and a lesson learned.<span>Simon Sinek</span>";
-    // document.getElementById("qoute-content").innerHTML = randomQoute;
+    const randomQoute = "<p>" + item.qoute + "<span>" + item.author + "</span>" + "</p>";
+    document.getElementById("qoute-content").innerHTML = randomQoute;
 }
