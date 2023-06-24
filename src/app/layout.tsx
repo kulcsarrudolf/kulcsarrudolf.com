2a3f7d4c88085d4d42936e19bca4d42f0f27295f
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-06-24 17:09:54 +0300

    Added keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,17 @@ export const metadata = {
   title: "Kulcsar Rudolf - Software Developer",
   description:
     "Welcome to my personal website! I'm Kulcsar Rudolf, a passionate software developer, and this is where I showcase my work, share my thoughts, and document my journey in the world of technology. Explore my portfolio, read my blog posts, and discover the projects I've been working on. Join me as I delve into the exciting realm of software development and share my insights, experiences, and learnings. Let's connect and explore the endless possibilities of coding together!",
+  keywords: [
+    "software developer",
+    "portfolio",
+    "blog",
+    "technology",
+    "coding",
+    "react",
+    "typescript",
+    "nextjs",
+    "nodejs",
+  ],
 };
 
 export default function RootLayout({
