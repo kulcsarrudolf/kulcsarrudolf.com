27be41983211b576d42853be2486d5f8f5ce0859
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2023-07-03 22:21:10 +0300

    Hide social media mobile devices

--- src/components/navbar/SocialMediaLinks.tsx
+++ src/components/navbar/SocialMediaLinks.tsx
@@ -1,18 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SocialMediaLink from "@/types/social-media-link";
 import { SOCIAL_MEDIA } from "./navber-links";
+import { useMediaQuery } from "react-responsive";
 
-export const SocialMediaLinks = () => (
-  <>
-    {SOCIAL_MEDIA.map((socialMedia: SocialMediaLink) => (
-      <a key={socialMedia.title} href={socialMedia.href} target="_blank">
-        <FontAwesomeIcon
-          icon={socialMedia.icon}
-          className="ml-4 text-white text-2xl"
-        />
-      </a>
-    ))}
-  </>
-);
+export const SocialMediaLinks = () => {
+  const isMobileView = useMediaQuery({ maxWidth: 450 });
 
+  if (isMobileView) return null;
+
+  return (
+    <>
+      {SOCIAL_MEDIA.map((socialMedia: SocialMediaLink) => (
+        <a key={socialMedia.title} href={socialMedia.href} target="_blank">
+          <FontAwesomeIcon
+            icon={socialMedia.icon}
+            className="ml-4 text-white text-2xl"
+          />
+        </a>
+      ))}
+    </>
+  );
+};
 export default SocialMediaLinks;
