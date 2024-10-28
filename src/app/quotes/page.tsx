843a2df36f8baacc79063cac944f99497b14dcb2
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2024-10-28 12:59:51 +0200

    Quotes page improvements

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,21 +1,27 @@
-import { Title, Paragraph } from "@/components/general/typography";
+import { Paragraph, Title } from "@/components/general/typography";
 import quotes from "@/components/quote/quotes";
 import Quote from "@/types/quote.type";
 
 const Quotes = () => {
   return (
     <>
-      <Title>My Favorite Quotes</Title>
-
-      {quotes.map((quote: Quote) => (
-        <div
-          className="border border-black-500 p-4 rounded-xl shadow-md mb-4"
-          key={quote.id}
-        >
-          <p className="text-xl font-medium	italic mb-4">{quote.quote}</p>
-          <p className="text-[#3b64b8] font-bold">{quote.author}</p>
-        </div>
-      ))}
+      <Title>Inspiring Thoughts</Title>
+      <Paragraph>
+        These quotes offer moments of clarity, encouragement, and perspective
+        from voices that resonate. They serve as reminders to stay grounded,
+        focused, and purposeful in everyday life.
+      </Paragraph>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {quotes.map((quote: Quote) => (
+          <div
+            className="border border-black-500 p-3 rounded-l shadow-md mb-4"
+            key={quote.id}
+          >
+            <p className="text-l font-medium italic mb-2">{quote.quote}</p>
+            <p className="text-[#3b64b8] font-bold text-sm">{quote.author}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
