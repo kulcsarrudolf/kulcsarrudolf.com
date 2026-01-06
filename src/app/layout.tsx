f7b40ec8f0fd5fdf6bb0ab61807b814158ac7700
kulcsarrudolf <kulcsarrudolf@gmail.com>
2026-01-06 11:33:25 +0200

    feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,34 @@ export const metadata = {
     "nextjs",
     "nodejs",
   ],
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "android-chrome-192x192",
+        url: "/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        rel: "android-chrome-512x512",
+        url: "/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+  manifest: "/favicon/site.webmanifest",
 };
 
 export default function RootLayout({
