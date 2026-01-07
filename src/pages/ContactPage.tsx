84877c069773365880b38b79231a136fbfcbc0c2
kulcsarrudolf <kulcsarrudolf@gmail.com>
2026-01-07 11:21:58 +0200

    feat: contact page update

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -6,16 +6,12 @@ import ContactForm from "@/components/contact/ContactForm";
 
 export default function ContactPage() {
   const { t } = useTranslation();
-  const email = t("contact.email");
 
   return (
     <>
       <Title>{t("contact.title")}</Title>
-      <Paragraph>
-        {t("contact.description", { email: <span className="font-bold">{email}</span> })}
-      </Paragraph>
+      <Paragraph>{t("contact.description")}</Paragraph>
       <ContactForm />
     </>
   );
 }
-
